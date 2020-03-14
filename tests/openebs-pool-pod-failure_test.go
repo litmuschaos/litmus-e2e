1a4a8de8e34439d6bd12a9200e6efdb941ea557a
UDIT GAURAV <35391335+uditgaurav@users.noreply.github.com>
2020-03-15 03:56:00 +0530

    Typo fix in experiments (#92)
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- tests/openebs-pool-pod-failure_test.go
+++ tests/openebs-pool-pod-failure_test.go
@@ -34,7 +34,6 @@ var (
 	startTimeBefore [3]*metav1.Time
 	image_tag       = os.Getenv("IMAGE_TAG")
 	engineName      = "engine1"
-	image_tag       = os.Getenv("IMAGE_TAG")
 	experimentName  = "openebs-pool-pod-failure"
 )
 
@@ -124,9 +123,9 @@ var _ = Describe("BDD test for openebs pool pod failure experiment", func() {
 			Expect(err).To(BeNil(), "Fail to create chaos experiment")
 			err = exec.Command("wget", "-O", "pool-pod-failure-exp.yaml", "https://hub.litmuschaos.io/api/chaos?file=charts/openebs/openebs-pool-pod-failure/experiment.yaml").Run()
 			Expect(err).To(BeNil(), "fail get chaos experiment")
-			err = exec.Command("sed", "-i", `s/ansible-runner:latest/ansible-runner:`+image_tag+`/g`, "pool-pod-failure-ce.yaml").Run()
+			err = exec.Command("sed", "-i", `s/ansible-runner:latest/ansible-runner:`+image_tag+`/g`, "pool-pod-failure-exp.yaml").Run()
 			Expect(err).To(BeNil(), "fail to edit chaos experiment yaml")
-			err = exec.Command("kubectl", "apply", "-f", "pool-pod-failure-ce.yaml", "-n", chaosTypes.ChaosNamespace).Run()
+			err = exec.Command("kubectl", "apply", "-f", "pool-pod-failure-exp.yaml", "-n", chaosTypes.ChaosNamespace).Run()
 			Expect(err).To(BeNil(), "fail to create chaos experiment")
 
 			fmt.Println("Chaos Experiment Created Successfully")
