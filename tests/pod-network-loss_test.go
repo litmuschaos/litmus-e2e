f5628f6dd831845597c83fcffdbf461f1d179bf2
Udit Gaurav <uditgaurav@gmail.com>
2020-03-31 20:56:06 +0530

    fix: Syntax error fix for generic experiments
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- tests/pod-network-loss_test.go
+++ tests/pod-network-loss_test.go
@@ -144,7 +144,8 @@ var _ = Describe("BDD of pod-network-loss experiment", func() {
 			//Fetch job pod name
 			By("Fetch Job pod name")
 			jobNamespace := chaosTypes.ChaosNamespace
-			jobPodLogs, err := utils.JobLogs(experimentName, jobNamespace, engineName, client)			Expect(jobPodLogs).To(Equal(0), "Fail to print the logs of the experiment")
+			jobPodLogs, err := utils.JobLogs(experimentName, jobNamespace, engineName, client)
+			Expect(jobPodLogs).To(Equal(0), "Fail to print the logs of the experiment")
 			Expect(err).To(BeNil(), "Fail to get the experiment job pod")
 
 			//Waiting for experiment job to get completed
