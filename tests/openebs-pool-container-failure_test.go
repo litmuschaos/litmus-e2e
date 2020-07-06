97f0ee7dc91374dc6db813725bf5d37b049e756a
Udit Gaurav <uditgaurav@gmail.com>
2020-07-06 19:34:39 +0530

    feat(table): Add Readme table for the openebs test in master branch
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- tests/openebs-pool-container-failure_test.go
+++ tests/openebs-pool-container-failure_test.go
@@ -267,7 +267,7 @@ var _ = Describe("BDD of openebs pool container failure experiment", func() {
 			chaosEngine, err := clientSet.ChaosEngines(chaosTypes.ChaosNamespace).Get(engineName, metav1.GetOptions{})
 			Expect(err).To(BeNil(), "Fail to get the chaosengine while updating the result in a table")
 			testVerdict := string(chaosEngine.Status.Experiments[0].Verdict)
-			err = utils.UpdateResultTable(experimentName, testVerdict, engineName, clientSet)
+			err = utils.UpdateResultTable("OpenEBS Pool Container Failure test", testVerdict, engineName, clientSet)
 			Expect(err).To(BeNil(), "Fail run the script for result updation")
 			fmt.Println("Result updated successfully !!!")
 		})
