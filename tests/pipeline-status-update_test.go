03b17b0946e8c25d9e5583402867ce2a4093b789
UDIT GAURAV <35391335+uditgaurav@users.noreply.github.com>
2020-07-20 10:48:32 +0530

    feat(coverage): Add coverage percentage in readme for openebs (#176)
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- tests/pipeline-status-update_test.go
+++ tests/pipeline-status-update_test.go
@@ -1,7 +1,9 @@
- 
 package tests
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/litmuschaos/litmus-e2e/pkg/utils"
@@ -28,7 +30,13 @@ var _ = Describe("BDD of pipeline status update", func() {
 			//Updating the pipeline status table
 			By("Updating the pipeline status table")
 			var err error
-			err = utils.UpdatePipelineStatus()
+			coverageData, err := ioutil.ReadFile("../coverage")
+			if err != nil {
+				klog.Infof("failed reading coverageData from file: %s", err)
+			}
+			lines := strings.Split(string(coverageData), "\n")
+			fmt.Printf("\nFile Content: %s\n", string(lines[0]))
+			err = utils.UpdatePipelineStatus(string(lines[0]))
 			Expect(err).To(BeNil(), "Fail run the script for pipeline status updation")
 			klog.Info("Pipeline status updated successfully !!!")
 
