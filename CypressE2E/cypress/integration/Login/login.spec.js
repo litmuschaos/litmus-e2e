a15c60de1c6c87c544f266d35c303431bb9cb20b
VEDANT SHROTRIA <40681425+Jonsy13@users.noreply.github.com>
2020-07-22 00:55:46 +0530

    Added Cypress Tests for LoginPage,Welcome Modal and Routes (Before and After Login) (#178)
    * [Cherry-pick to litmus-portal]: Contains cypress tests (#167)
    
    * Added Cypress E2E folder and basic configurations (#162)
    
    * Added Cypress E2E Folder and basic configurations
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    * Test Description fixes
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    * Added Configurations for CI
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    * fix(Server): Add a fix to run the Server in background in GitLab (#166)
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    * Added Some Fixes for Gitlab configurations of Cypress E2E
    
    * fix(cluster-connect): Add correct username for cluster connect
    
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>
    
    Co-authored-by: VEDANT SHROTRIA <40681425+Jonsy13@users.noreply.github.com>
    
    * Testing on my branch
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    Testing on my branch
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    Testing on my branch Jonsy
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    Testing on my branch Jonsy
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    Added Reporter
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    Added Tests for Login Page,Community,Homepage,Welcome Modal and Routes
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    * Added Some Comments for custom commands and minor changes.
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    * Fixed the typo.
    
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>
    
    Co-authored-by: UDIT GAURAV <35391335+uditgaurav@users.noreply.github.com>

--- CypressE2E/cypress/integration/Login/login.spec.js
+++ CypressE2E/cypress/integration/Login/login.spec.js
@@ -3,9 +3,54 @@
 describe("Testing the accessibility of Login page",()=>{
     
     it("Visiting the Login Page",()=>{
+        indexedDB.deleteDatabase('localforage');
         cy.visit("/login");
+        cy.url().should('include','/login');
         cy.log("Visited the Login page Successfully");
     });
-    
+
+})
+
+describe("Checking functionality of Login Page",()=>{
+   
+    beforeEach("Visit Login Page",()=>{
+        cy.visit('/login');
+    })
+  
+    it("Checking Input areas functionallity",()=>{
+        cy.login("Vedant","Litmus");
+        cy.get("[data-cy=inputEmail] input").should("have.value","Vedant");
+        cy.get("[data-cy=inputPassword] input").should("have.value","Litmus");
+    })
+
+    it("Testing the only single input sign in [ Should not be possible ]",()=>{
+        cy.loginServer(503);
+        cy.login("Vedant"," ");
+        cy.url().should('include','/login');
+        cy.get('[data-cy=inputEmail] input').clear();
+        cy.get('[data-cy=inputPassword] input').clear();
+        cy.login(" ","Litmus");
+        cy.url().should('include','/login');
+    })
+
+    it("Testing with wrong details [ Should not be possible ]",()=>{
+        cy.loginServer(503);
+        cy.login("Vedant","Litmus");
+        cy.url().should('include','/login');
+    })
+
+    it("Testing with without any details [ Should not be possible ]",()=>{
+        cy.loginServer(503);
+        cy.login(" "," ");
+        cy.url().should('include','/login');
+    })
+
+    it("Testing with Correct details [ Must redirect to Welcome modal ]",()=>{
+        cy.loginServer(200);
+        cy.login("Vedant","Litmus");
+        cy.contains("Welcome to Litmus Portal");
+        indexedDB.deleteDatabase('localforage');
+    })
+
 })
 
--- CypressE2E/cypress/support/commands.js
+++ CypressE2E/cypress/support/commands.js
@@ -1,25 +1,44 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+/// <reference types="Cypress" />
+
+//This Script provides custom commands for tests.
+
+/* loginServer() command for stubbing a server 
+with /auth/login route with any status code provided as argument.*/
+Cypress.Commands.add('loginServer',(loginStatus)=>{
+    cy.server();
+    cy.route({
+        method: 'POST',
+        url: '/auth/login',
+        status: loginStatus,
+        response: (loginStatus==503)?'':{} 
+    });
+})
+
+/* modalServer() command for stubbing a server 
+with /auth/update route with any status code provided as argument.*/
+Cypress.Commands.add('modalServer',(modalStatus)=>{
+    cy.server();
+    cy.route({
+        url:'/auth/update',
+        method: 'POST',
+        status: modalStatus,
+        response: (modalStatus==503)?'':{email: "Vedant@Gmail.com",name: "Vedant"}
+    })
+})
+
+// Custom command for Inputting details in Welcome Modal.
+Cypress.Commands.add('welcomeModalInputs',(ProjectName,Email,Password)=>{
+    cy.get('[data-cy=inputProjectEmail] input').type(ProjectName);
+    cy.get('[data-cy=Welcome-continue]').click();
+    cy.get('[data-cy=inputProjectName] input').type(Email)
+    cy.get('[data-cy=Welcome-continue]').click();
+    cy.get('[data-cy=inputProjectPassword]').type(Password);
+    cy.get('[data-cy=selectProjectFinish]').click();
+})
+
+//Command for inputting details and login.
+Cypress.Commands.add('login',(Email,password)=>{
+    cy.get('[data-cy=inputEmail] input').type(Email);
+    cy.get('[data-cy=inputPassword] input').type(password);
+    cy.get('[data-cy=loginButton]').click();
+})
