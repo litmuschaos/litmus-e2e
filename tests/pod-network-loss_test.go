d14e40447bf04d2ed3583789e0fe5eb74e8bff60
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2020-10-28 13:39:56 +0530

    chore(abort): Add abort test for all supported experiments (#206)
    Signed-off-by: Udit Gaurav <udit.gaurav@mayadata.io>

--- tests/pod-network-loss_test.go
+++ tests/pod-network-loss_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"log"
 	"testing"
+	"time"
 
 	"github.com/litmuschaos/litmus-e2e/pkg"
 	"github.com/litmuschaos/litmus-e2e/pkg/environment"
@@ -118,6 +119,85 @@ var _ = Describe("BDD of pod-network-loss experiment", func() {
 		})
 	})
 
+	// BDD TEST CASE 2
+	//Add abort-chaos for pod-network-loss experiment
+	Context("Abort-Chaos check of litmus component", func() {
+
+		It("Should check the abort of pod-network-loss experiment", func() {
+
+			testsDetails := types.TestDetails{}
+			clients := environment.ClientSets{}
+
+			klog.Info("RUNNING POD-NETWORK-LOSS ABORT CHAOS TEST!!!")
+			//Getting kubeConfig and Generate ClientSets
+			By("[PreChaos]: Getting kubeconfig and generate clientset")
+			err := clients.GenerateClientSetFromKubeConfig()
+			Expect(err).To(BeNil(), "Unable to Get the kubeconfig, due to {%v}", err)
+
+			//Fetching all the default ENV
+			By("[PreChaos]: Fetching all default ENVs")
+			klog.Infof("[PreReq]: Getting the ENVs for the %v test", testsDetails.ExperimentName)
+			environment.GetENV(&testsDetails, "pod-network-loss", "pod-net-loss-abort")
+
+			// Checking the chaos operator running status
+			By("[Status]: Checking chaos operator status")
+			err = pkg.OperatorStatusCheck(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Operator status check failed, due to {%v}", err)
+
+			//Installing RBAC for the experiment
+			By("[Install]: Installing RBAC")
+			err = pkg.InstallGoRbac(&testsDetails, testsDetails.ChaosNamespace)
+			Expect(err).To(BeNil(), "Fail to install rbac, due to {%v}", err)
+
+			//Installing Chaos Experiment for pod-delete
+			By("[Install]: Installing chaos experiment")
+			err = pkg.InstallGoChaosExperiment(&testsDetails, testsDetails.ChaosNamespace)
+			Expect(err).To(BeNil(), "Fail to install chaos experiment, due to {%v}", err)
+
+			//Installing Chaos Engine for pod-delete
+			By("[Install]: Installing chaos engine")
+			err = pkg.InstallGoChaosEngine(&testsDetails, testsDetails.ChaosNamespace)
+			Expect(err).To(BeNil(), "Fail to install chaosengine, due to {%v}", err)
+
+			//Checking runner pod running state
+			By("[Status]: Runner pod running status check")
+			_, err = pkg.RunnerPodStatus(&testsDetails, testsDetails.AppNS, clients)
+			Expect(err).To(BeNil(), "Runner pod status check failed, due to {%v}", err)
+
+			//Chaos pod running status check
+			err = pkg.ChaosPodStatus(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Chaos pod status check failed, due to {%v}", err)
+
+			//Waiting for chaosresult creation from experiment
+			klog.Info("[Wait]: waiting for chaosresult creation from experiment")
+			time.Sleep(15 * time.Second)
+
+			//Abort the chaos experiment
+			By("[Abort]: Abort the chaos by patching engine state")
+			err = pkg.ChaosAbort(&testsDetails)
+			Expect(err).To(BeNil(), "[Abort]: Chaos abort failed, due to {%v}", err)
+
+			//Waiting for chaos pod to get completed
+			//But the chaos should be aborted before hand
+			By("[Status]: Wait for chaos pod completion and then print logs")
+			err = pkg.ChaosPodLogs(&testsDetails, clients)
+			Expect(err).NotTo(BeNil(), "[TEST FAILED]: Unable to remove chaos pod after abort")
+
+			//Checking the chaosresult verdict
+			By("[Verdict]: Checking the chaosresult verdict")
+			chaosResult, err := pkg.GetChaosResultVerdict(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Fail to get the chaosresult Verdict, due to {%v}", err)
+			Expect(chaosResult).To(Equal("Stopped"), "ChasoResult Verdict is not Stopped, due to {%v}", err)
+
+			//Checking chaosengine verdict
+			By("Checking the Verdict of Chaos Engine")
+			chaosEngineVerdict, err := pkg.GetChaosEngineVerdict(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Fail to get the chaosengine Verdict, due to {%v}", err)
+			Expect(chaosEngineVerdict).To(Equal("Stopped"), "ChaosEngine Verdict is not Stopped, due to {%v}", err)
+
+		})
+	})
+
 	// BDD for pipeline result update
 	Context("Check for the result update", func() {
 
@@ -139,11 +219,21 @@ var _ = Describe("BDD of pod-network-loss experiment", func() {
 
 			//Updating the pipeline result table
 			By("Updating the pipeline result table")
-			//Getting chaosengine verdict
-			By("Getting Verdict of Chaos Engine")
+			//Getting chaosengine verdict for experiment test
+			By("Getting Verdict of Chaos Engine for experiment test")
 			ChaosEngineVerdict, err := pkg.GetChaosEngineVerdict(&testsDetails, clients)
 			Expect(err).To(BeNil(), "ChaosEngine Verdict check failed, due to {%v}", err)
 			Expect(ChaosEngineVerdict).NotTo(BeEmpty(), "Fail to get chaos engine verdict, due to {%v}", err)
+
+			//Getting chaosengine verdict for abort test
+			By("Getting Verdict of Chaos Engine for abort test")
+			testsDetails.EngineName = "pod-net-loss-abort"
+			ChaosEngineVerdictForAbort, err := pkg.GetChaosEngineVerdict(&testsDetails, clients)
+			Expect(err).To(BeNil(), "ChaosEngine Verdict check failed, due to {%v}", err)
+			if ChaosEngineVerdictForAbort != "Stopped" {
+				ChaosEngineVerdict = "Fail"
+				klog.Error("Abort chaos test verdict is not Pass")
+			}
 
 			err = pkg.UpdateResultTable("Inject Packet Loss Into Application Pod", ChaosEngineVerdict, &testsDetails)
 			Expect(err).To(BeNil(), "Job Result Updation failed, due to {%v}", err)
