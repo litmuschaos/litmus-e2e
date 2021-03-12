cee70af14e9d6799977669cc3fc093e960194554
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-03-12 20:05:23 +0530

    Added gitops tests.
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- Cypress/cypress/integration/Parallel_Tests/Create_Workflow/gitopsWorkflow.spec.js
+++ Cypress/cypress/integration/Parallel_Tests/Create_Workflow/gitopsWorkflow.spec.js
@@ -14,5 +14,19 @@ describe("Testing the create Workflow Utility", () => {
   it("Checking accessiblity of GitOps Panel", () => {
     cy.visit("/settings");
     cy.get("[data-cy=gitOps]").click();
+    cy.get("[data-cy=localRadioButton] input[type=radio]").should("be.checked");
+    cy.get("[data-cy=gitopsRadioButton] input[type=radio]").should(
+      "not.be.checked"
+    );
+    cy.get("[data-cy=connectButton] button").should("be.disabled");
+    cy.get("[data-cy=gitopsRadioButton] input[type=radio]").check();
+    cy.get("[data-cy=connectButton] button").should("be.enabled");
+    cy.get("[data-cy=githubURLInput] input").clear().type("URL");
+    cy.get("[data-cy=githubBranchInput] input").clear().type("Branch");
+    cy.get("[data-cy=accessTokenRadioButton] input[type=radio]").check();
+    cy.get("[data-cy=accessTokenInput] input").should("be.visible");
+    cy.get("[data-cy=sshKeyRadioButton] input[type=radio]").check();
+    cy.get("[data-cy=sshKeyBox]").should("be.visible");
+    cy.get("[data-cy=sshKeyCopyButton] button").click();
   });
 });
