2aa132889158cd4b3cc07ec0fd49f654d78369ae
Oum Kale <oumkale@chaosnative.com>
2021-04-15 18:48:41 +0530

    coverage added
    Signed-off-by: Oum Kale <oumkale@chaosnative.com>

--- utils/pipeline_status_update.py
+++ utils/pipeline_status_update.py
@@ -100,9 +100,9 @@ def fetch_file_content():
     updated_file_content =  ('\n').join(content_list)
 
     # updating result's table if the table is already present
-    if file_content.find('<table>\n <tr>\n  <th>Pipeline ID</th>\n  <th>Execution Time</th>\n  <th>'+version_name+'</th>\n <th>Coverage</th>\n </tr>\n')>0:
-        new_pipeline = ' <tr>\n  <td>{}</td>\n  <td>{}</td>\n  <td>{}</td>\n<td>{}</td> </tr>\n'.format(pipeline_url,time_stamp,tag,totalCoverage)
-        index = content_list.index('  <th>'+version_name+'</th>')
+    if file_content.find('<table>\n <tr>\n  <th>Pipeline ID</th>\n  <th>Execution Time</th>\n  <th>'+version_name+'</th></tr>\n')>0:
+        new_pipeline = ' <tr>\n  <td>{}</td>\n  <td>{}</td>\n  <td>{}</td>\n </tr>\n'.format(pipeline_url,time_stamp,tag)
+        index = content_list.index('  <th>'+version_name+'</th></tr>')
         content_list.insert(index+2,new_pipeline)
         for line in content_list:
                count += line.count("<tr>")  
@@ -116,8 +116,8 @@ def fetch_file_content():
 
     # creating result's table for first pipeline result entry 
     else:
-        updated_file_content =  '<table>\n <tr>\n  <th>Pipeline ID</th>\n  <th>Execution Time</th>\n  <th>'+version_name+'</th>\n <th>Coverage</th>\n </tr>\n'
-        updated_file_content = updated_file_content + (' <tr>\n  <td>{}</td>\n  <td>{}</td>\n  <td>{}</td>\n <td>{}</td>\n </tr>\n'.format(pipeline_url,time_stamp,tag,totalCoverage))
+        updated_file_content =  '<table>\n <tr>\n  <th>Pipeline ID</th>\n  <th>Execution Time</th>\n  <th>'+version_name+'</th></tr>\n'
+        updated_file_content = updated_file_content + (' <tr>\n  <td>{}</td>\n  <td>{}</td>\n  <td>{}</td>\n </tr>\n'.format(pipeline_url,time_stamp,tag))
         updated_file_content = updated_file_content + '</table>'
         index = len(content_list)
         content_list.insert(index, updated_file_content)
