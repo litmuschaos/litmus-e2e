e93352d8fb0d7b01a605911e6dc71bee78b87750
VEDANT SHROTRIA <40681425+Jonsy13@users.noreply.github.com>
2021-05-12 20:42:26 +0530

    Added Github-Action workflows for Nightly Builds and Gitlab Cleanup for Generic Pipeline. (#264)
    * Added Changes required for website updates.
    
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- tests/pipeline-update_test.go
+++ tests/pipeline-update_test.go
@@ -1,11 +1,6 @@
 package tests
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"os/exec"
-	"strings"
 	"testing"
 
 	"github.com/litmuschaos/litmus-e2e/pkg"
@@ -30,35 +25,20 @@ var _ = Describe("BDD of pipeline status update", func() {
 	Context("Check for the overall pipeline update", func() {
 
 		testsDetails := types.TestDetails{}
+		environment.GetENV(&testsDetails, "pipeline-update", "")
 		if testsDetails.UpdateWebsite == "true" {
 			It("Should check for the result updation", func() {
 
 				testsDetails := types.TestDetails{}
 				var err error
-				var out, stderr bytes.Buffer
 
 				//Fetching all the default ENV
 				By("[PreChaos]: Fetching all default ENVs")
 				klog.Infof("[PreReq]: Getting the ENVs for the %v test", testsDetails.ExperimentName)
 				environment.GetENV(&testsDetails, "pipeline-update", "")
 
-				//Setup metrics to get pipeline details
-				//Creating rbac
-				cmd := exec.Command("bash", "../metrics/e2e-metrics", "start")
-				cmd.Stdout = &out
-				cmd.Stderr = &stderr
-				err = cmd.Run()
-				klog.Infof(fmt.Sprint(err) + ": " + stderr.String())
-				Expect(err).To(BeNil(), "Fail to setup metrics for pipeline, due to {%v}", err)
-
-				//Updating the pipeline status table
-				By("Updating the pipeline status table")
-				coverageData, err := ioutil.ReadFile("e2e-metrics/coverage")
-				Expect(err).To(BeNil(), "failed reading coverageData from file, due to {%v}", err)
-
-				lines := strings.Split(string(coverageData), "\n")
-				klog.Infof("\nFile Content: %s\n", string(lines[0]))
-				err = pkg.UpdatePipelineStatus(&testsDetails, string(lines[0]))
+				// TODO: Currently coverageData is hardcoded as 60%, Need to be changed to actual coverage.
+				err = pkg.UpdatePipelineStatus(&testsDetails, "60")
 				Expect(err).To(BeNil(), "Fail to run the script for pipeline status update,due to {%v}", err)
 				klog.Info("Pipeline status updated successfully !!!")
 
@@ -66,17 +46,7 @@ var _ = Describe("BDD of pipeline status update", func() {
 
 			It("Should check for the result updation", func() {
 
-				var err error
-				var out, stderr bytes.Buffer
-				//Setup metrics to get pipeline details
-				//Creating rbac
-				cmdStop := exec.Command("bash", "../metrics/e2e-metrics", "stop")
-				cmdStop.Stdout = &out
-				cmdStop.Stderr = &stderr
-				err = cmdStop.Run()
-				klog.Infof(fmt.Sprint(err) + ": " + stderr.String())
-				Expect(err).To(BeNil(), "Fail to setup metrics for pipeline, due to {%v}", err)
-
+				//TODO: Need new logic for checking the result updation
 			})
 		} else {
 			klog.Info("[SKIP]: Skip updating the result on website")
--- utils/pipeline_status_update.py
+++ utils/pipeline_status_update.py
@@ -80,7 +80,7 @@ content_list = file_content.split('\n')
 file_update_retries = 5
 
 # github pipeline url using pipeline_id
-pipeline_url ="<a href= \"https://gitlab.mayadata.io/litmuschaos/litmus-e2e/pipelines/{0}\">{0}</a>".format(pipeline_id)
+pipeline_url ="<a href= \"https://github.com/litmuschaos/litmus-e2e/actions/runs/{0}\">{0}</a>".format(pipeline_id)
 
 def fetch_file_content():
     # fetching file contents of github file_path
