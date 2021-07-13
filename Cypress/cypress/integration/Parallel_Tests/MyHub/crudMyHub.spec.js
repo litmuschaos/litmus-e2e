32af01dc27574b22623a8653a907a5b6a0a7f966
Amit Kumar Das <40661238+amityt@users.noreply.github.com>
2021-07-13 11:41:20 +0530

    Added e2e tests for MyHub (#282)
    * Added crud operation tests for MyHub
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>

---
+++ Cypress/cypress/integration/Parallel_Tests/MyHub/crudMyHub.spec.js
@@ -0,0 +1,105 @@
+/// <reference types="Cypress" />
+import * as user from "../../../fixtures/Users.json";
+
+describe("Testing CRUD operation with MyHub", () => {
+	before(
+		"Clearing the cookies, login as admin and visiting the MyHub route",
+		() => {
+			cy.requestLogin(user.AdminName, user.AdminPassword);
+			cy.waitForCluster("Self-Agent");
+			cy.visit("/myhub");
+		}
+	);
+
+	it("Adding a new MyHub with incorrect details", () => {
+		cy.get("[data-cy=myHubConnectButton]").click();
+		cy.wait(1000);
+		cy.get("[data-cy=hubName] input").clear().type("my-test-hub");
+		cy.get("[data-cy=githubURLInput]")
+			.find("input")
+			.eq(0)
+			.clear()
+			.type("test-myhub.com");
+		cy.get("[data-cy=githubBranchInput]")
+			.find("input")
+			.eq(0)
+			.clear()
+			.type("master");
+		cy.get("[data-cy=MyHubSubmit]").should("be.disabled");
+		cy.get("[data-cy=cancel]").click();
+	});
+
+	it("Adding a new MyHub with correct details", () => {
+		cy.GraphqlWait("addMyHub", "addNewMyHub");
+		cy.get("[data-cy=myHubConnectButton]").click();
+		cy.wait(1000);
+		cy.get("[data-cy=hubName] input").clear().type("my-test-hub");
+		cy.get("[data-cy=githubURLInput]")
+			.find("input")
+			.eq(0)
+			.clear()
+			.type("https://github.com/litmuschaos/chaos-charts");
+		cy.get("[data-cy=githubBranchInput]")
+			.find("input")
+			.eq(0)
+			.clear()
+			.type("master");
+		cy.get("[data-cy=MyHubSubmit]")
+			.should("be.enabled")
+			.click()
+			.then(() => {
+				cy.wait("@addNewMyHub").its("response.statusCode").should("eq", 200);
+			});
+		cy.get("[data-cy=myHubAlert]").should("be.visible");
+	});
+
+	it("Update an existing MyHub details (Update operation)", () => {
+		cy.GraphqlWait("updateMyHub", "updateExistingMyHub");
+		cy.get("[data-cy=myHubCardOption]").eq(1).click();
+		cy.get("[data-cy=myHubOptions]").should("be.visible");
+		cy.get("[data-cy=editHub]").eq(1).click();
+		cy.get("[data-cy=hubName]")
+			.find("input")
+			.eq(0)
+			.should("be.visible")
+			.clear()
+			.type("my-test-hub-updated");
+		cy.get("[data-cy=githubURLInput]")
+			.find("input")
+			.eq(0)
+			.clear()
+			.type("https://github.com/litmuschaos/chaos-charts", { force: true });
+		cy.get("[data-cy=githubBranchInput]")
+			.find("input")
+			.eq(0)
+			.clear()
+			.type("v1.13.x");
+		cy.get("[data-cy=MyHubSubmit]")
+			.should("be.enabled")
+			.click()
+			.then(() => {
+				cy.wait("@updateExistingMyHub")
+					.its("response.statusCode")
+					.should("eq", 200);
+			});
+		cy.get("[data-cy=myHubAlert]").should("be.visible");
+	});
+
+	it("Deleting a MyHub", () => {
+		cy.GraphqlWait("deleteMyHub", "deleteExisitingMyHub");
+		cy.get("[data-cy=myHubCardOption]").eq(1).click();
+		cy.get("[data-cy=myHubOptions]").should("be.visible");
+		cy.get("[data-cy=myHubDelete]")
+			.contains("p", "Disconnect Hub")
+			.then(($div) => {
+				cy.wrap($div).click({ force: true });
+			});
+		cy.get("[data-cy=deleteHubModal]")
+			.should("be.visible")
+			.find("button")
+			.eq(2)
+			.then(($div) => {
+				cy.wrap($div).click();
+			});
+	});
+});
