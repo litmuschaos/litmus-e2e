db60f5a50bccd3e9cf61ef74380abded88ef0fba
Oum Kale <oumkale@chaosnative.com>
2021-07-15 10:54:55 +0530

    updating engine names in history test check
    Signed-off-by: Oum Kale <oumkale@chaosnative.com>

--- pkg/environment/environment.go
+++ pkg/environment/environment.go
@@ -35,8 +35,8 @@ func GetENV(testDetails *types.TestDetails, expName, engineName string) {
 	testDetails.EBSVolumeTag = Getenv("EBS_VOLUME_TAG", "")
 	testDetails.Region = Getenv("REGION", "us-west-1")
 	testDetails.UpdateWebsite = Getenv("UPDATE_WEBSITE", "false")
-	testDetails.TargetNodes = Getenv("TARGET_NODES", "minikube")
-	testDetails.NodeLabel = Getenv("NODE_LABEL", "beta.kubernetes.io/arch=amd64")
+	testDetails.TargetNodes = Getenv("TARGET_NODES", "")
+	testDetails.NodeLabel = Getenv("NODE_LABEL", "")
 
 	//All Images for running chaos test
 	testDetails.AnsibleExperimentImage = Getenv("ANSIBLE_EXPERIMENT_IMAGE", "litmuschaos/ansible-runner:ci")
