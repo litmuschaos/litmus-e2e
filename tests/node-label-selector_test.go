296ad234c6813c5f241716535195b0543f6d56a7
neelanjan00 <neelanjanmanna@gmail.com>
2021-07-15 20:26:10 +0530

    Added test cases for target node selection by node label

--- tests/node-label-selector_test.go
+++ tests/node-label-selector_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1"
@@ -22,7 +23,7 @@ func TestGoNodeLabelSelector(t *testing.T) {
 
 var _ = Describe("BDD of Node Label selector", func() {
 	Context("Test for target node selection by node name", func() {
-		It("Should execute the node-cpu-hog experiment using a valid TARGET_NODES value", func() {
+		It("Execute the node-cpu-hog experiment using a valid TARGET_NODES value", func() {
 			testsDetails := types.TestDetails{}
 			clients := environment.ClientSets{}
 			chaosEngine := v1alpha1.ChaosEngine{}
@@ -77,7 +78,7 @@ var _ = Describe("BDD of Node Label selector", func() {
 			Expect(err).To(BeNil(), "ChaosEngine Verdict check failed, due to {%v}", err)
 		})
 
-		It("Should execute the node-cpu-hog experiment using an invalid TARGET_NODES value", func() {
+		It("Execute the node-cpu-hog experiment using an invalid TARGET_NODES value", func() {
 			testsDetails := types.TestDetails{}
 			clients := environment.ClientSets{}
 			chaosEngine := v1alpha1.ChaosEngine{}
@@ -97,7 +98,130 @@ var _ = Describe("BDD of Node Label selector", func() {
 			environment.GetENV(&testsDetails, "node-cpu-hog", "go-engine4")
 
 			// Set incorrect target node string
-			testsDetails.TargetNodes = utils.GetRandomString(12)
+			testsDetails.TargetNodes = utils.GetRandomString(20)
+
+			// Checking the chaos operator running status
+			By("[Status]: Checking chaos operator status")
+			err = pkg.OperatorStatusCheck(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Operator status check failed, due to {%v}", err)
+
+			// Prepare Chaos Execution
+			By("[Prepare]: Prepare Chaos Execution")
+			err = pkg.PrepareChaos(&testsDetails, &chaosExperiment, &chaosEngine, clients, false)
+			Expect(err).To(BeNil(), "fail to prepare chaos, due to {%v}", err)
+
+			//Checking runner pod running state
+			By("[Status]: Runner pod running status check")
+			_, err = pkg.RunnerPodStatus(&testsDetails, testsDetails.AppNS, clients)
+			Expect(err).To(BeNil(), "Runner pod status check failed, due to {%v}", err)
+
+			// Chaos pod running status check
+			err = pkg.ChaosPodStatus(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Chaos pod status check failed, due to {%v}", err)
+
+			// Waiting for chaos pod to get completed
+			// And Print the logs of the chaos pod
+			By("[Status]: Wait for chaos pod completion and then print logs")
+			err = pkg.ChaosPodLogs(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Fail to get the experiment chaos pod logs, due to {%v}", err)
+
+			// Checking the chaosresult verdict
+			By("[Verdict]: Checking the chaosresult verdict")
+			chaosResult, err := pkg.GetChaosResultVerdict(&testsDetails, clients)
+			Expect(err).To(BeNil(), "ChasoResult Verdict check failed, due to {%v}", err)
+			Expect(chaosResult).To(Equal("Fail"), "ChaosResult verdict is not set to Fail, due to {%v}", err)
+
+			// Checking chaosengine verdict
+			By("Checking the Verdict of Chaos Engine")
+			chaosEngineVerdict, err := pkg.GetChaosEngineVerdict(&testsDetails, clients)
+			Expect(err).To(BeNil(), "ChaosEngine Verdict check failed, due to {%v}", err)
+			Expect(chaosEngineVerdict).To(Equal("Fail"), "ChaosEngine Verdict is not set to Fail, due to {%v}", err)
+		})
+	})
+
+	Context("Test for target node selection by node label", func() {
+		It("Executes the node-cpu-hog experiment using a valid NODE_LABEL value", func() {
+			testsDetails := types.TestDetails{}
+			clients := environment.ClientSets{}
+			chaosEngine := v1alpha1.ChaosEngine{}
+			chaosExperiment := v1alpha1.ChaosExperiment{}
+
+			//Getting kubeConfig and Generate ClientSets
+			By("[PreChaos]: Getting kubeconfig and generate clientset")
+			err := clients.GenerateClientSetFromKubeConfig()
+			Expect(err).To(BeNil(), "Unable to Get the kubeconfig, due to {%v}", err)
+
+			testsDetails.ExperimentName = "node-cpu-hog"
+
+			//Fetching all the default ENV
+			//Note: please don't provide custom experiment name here
+			By("[PreChaos]: Fetching all default ENVs")
+			klog.Infof("[PreReq]: Getting the ENVs for the %v test", testsDetails.ExperimentName)
+			environment.GetENV(&testsDetails, "node-cpu-hog", "go-engine4")
+
+			// Set TargetNodes to an empty string in order to use NodeLabel
+			testsDetails.TargetNodes = ""
+
+			// Checking the chaos operator running status
+			By("[Status]: Checking chaos operator status")
+			err = pkg.OperatorStatusCheck(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Operator status check failed, due to {%v}", err)
+
+			// Prepare Chaos Execution
+			By("[Prepare]: Prepare Chaos Execution")
+			err = pkg.PrepareChaos(&testsDetails, &chaosExperiment, &chaosEngine, clients, false)
+			Expect(err).To(BeNil(), "fail to prepare chaos, due to {%v}", err)
+
+			//Checking runner pod running state
+			By("[Status]: Runner pod running status check")
+			_, err = pkg.RunnerPodStatus(&testsDetails, testsDetails.AppNS, clients)
+			Expect(err).To(BeNil(), "Runner pod status check failed, due to {%v}", err)
+
+			//Chaos pod running status check
+			err = pkg.ChaosPodStatus(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Chaos pod status check failed, due to {%v}", err)
+
+			// Waiting for chaos pod to get completed
+			// And Print the logs of the chaos pod
+			By("[Status]: Wait for chaos pod completion and then print logs")
+			err = pkg.ChaosPodLogs(&testsDetails, clients)
+			Expect(err).To(BeNil(), "Fail to get the experiment chaos pod logs, due to {%v}", err)
+
+			// Checking the chaosresult verdict
+			By("[Verdict]: Checking the chaosresult verdict")
+			err = pkg.ChaosResultVerdict(&testsDetails, clients)
+			Expect(err).To(BeNil(), "ChasoResult Verdict check failed, due to {%v}", err)
+
+			// Checking chaosengine verdict
+			By("Checking the Verdict of Chaos Engine")
+			err = pkg.ChaosEngineVerdict(&testsDetails, clients)
+			Expect(err).To(BeNil(), "ChaosEngine Verdict check failed, due to {%v}", err)
+		})
+
+		It("Should execute the node-cpu-hog experiment using an invalid NODE_LABEL value", func() {
+			testsDetails := types.TestDetails{}
+			clients := environment.ClientSets{}
+			chaosEngine := v1alpha1.ChaosEngine{}
+			chaosExperiment := v1alpha1.ChaosExperiment{}
+
+			//Getting kubeConfig and Generate ClientSets
+			By("[PreChaos]: Getting kubeconfig and generate clientset")
+			err := clients.GenerateClientSetFromKubeConfig()
+			Expect(err).To(BeNil(), "Unable to Get the kubeconfig, due to {%v}", err)
+
+			testsDetails.ExperimentName = "node-cpu-hog"
+
+			//Fetching all the default ENV
+			//Note: please don't provide custom experiment name here
+			By("[PreChaos]: Fetching all default ENVs")
+			klog.Infof("[PreReq]: Getting the ENVs for the %v test", testsDetails.ExperimentName)
+			environment.GetENV(&testsDetails, "node-cpu-hog", "go-engine4")
+
+			// Set TargetNodes to an empty string in order to use NodeLabel
+			testsDetails.TargetNodes = ""
+
+			// Set an incorrect NodeLabel
+			testsDetails.NodeLabel = fmt.Sprintf("%s=%s", utils.GetRandomString(5), utils.GetRandomString(20))
 
 			// Checking the chaos operator running status
 			By("[Status]: Checking chaos operator status")
