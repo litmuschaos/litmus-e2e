d2ed8a70be646e3d5c90358a36bd6e45f2eea1f1
udit <udit@chaosnative.com>
2021-08-05 16:26:52 +0530

    Fix AWS Pipeline
    Signed-off-by: udit <udit@chaosnative.com>

--- components/experiment/experiment-image_test.go
+++ components/experiment/experiment-image_test.go
@@ -99,15 +99,15 @@ var _ = Describe("BDD of experiment image test", func() {
 
 		})
 	})
-	// // BDD for cleaning all components
-	// Context("Check for litmus components", func() {
+	// BDD for cleaning all components
+	Context("Check for litmus components", func() {
 
-	// 	It("Should delete all the litmus CRs", func() {
-	// 		By("[Cleanup]: Removing Litmus Components")
-	// 		err := pkg.Cleanup()
-	// 		Expect(err).To(BeNil(), "Fail to delete all litmus components, due to {%v}", err)
+		It("Should delete all the litmus CRs", func() {
+			By("[Cleanup]: Removing Litmus Components")
+			err := pkg.Cleanup()
+			Expect(err).To(BeNil(), "Fail to delete all litmus components, due to {%v}", err)
 
-	// 	})
+		})
 
-	// })
+	})
 })
