bfe25dd65254835d52368c02c35450765452d32a
udit <udit@chaosnative.com>
2021-08-20 15:26:12 +0530

    [Migrate]: Pod level pipeline from self hosted runner to shared runner
    Signed-off-by: udit <udit@chaosnative.com>

---
+++ build/debug.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "#############################################################"
+echo "############ Getting Node Status ################"
+echo "#############################################################"
+echo 
+echo "kubectl get nodes"
+kubectl get nodes || true
+
+echo "#############################################################"
+echo "############ Getting pods in Chaos Namespace ################"
+echo "#############################################################"
+echo 
+echo "kubectl get pods -n ${CHAOS_NAMESPACE}"
+kubectl get pods -n ${CHAOS_NAMESPACE} || true
+echo
+echo "###################################################################"
+echo "############ Getting pods in Application Namespace ################"
+echo "###################################################################"
+echo
+echo "kubectl get pods -n ${APP_NS}"
+kubectl get pods -n ${APP_NS} || true
+echo
+echo "################################################################"
+echo "############ Getting Chaos Resource Information ################"
+echo "################################################################"
+echo
+echo "kubectl get chaosengine,chaosexperiment,chaosresult -n ${CHAOS_NAMESPACE}"
+kubectl get chaosengine,chaosexperiment,chaosresult -n ${CHAOS_NAMESPACE} || true
+echo
+echo "################################################################"
+echo "############ Getting Service Account Information ################"
+echo "################################################################"
+echo
+echo "kubectl get sa -n ${CHAOS_NAMESPACE}"
+kubectl get sa -n ${CHAOS_NAMESPACE} || true
+echo
+echo "###################################################"
+echo "############ Describe ChaosEngine  ################"
+echo "###################################################"
+echo
+echo "kubectl describe chaosengine -n ${CHAOS_NAMESPACE}"
+kubectl describe chaosengine -n ${CHAOS_NAMESPACE} || true
+echo
+echo "###################################################"
+echo "############ Describe ChaosResult  ################"
+echo "###################################################"
+echo
+echo "kubectl describe chaosresult -n ${CHAOS_NAMESPACE}"
+kubectl describe chaosresult -n ${CHAOS_NAMESPACE} || true
+echo
