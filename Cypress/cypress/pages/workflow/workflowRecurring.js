31e9aaa2aeecc88a66892c015ab391f6f3b3484d
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-11-15 20:24:10 +0530

    Refactor wait statements for heatmap and bargraph
    Signed-off-by: DhananjayPurohit <dhananjaypurohit7@gmail.com>

--- Cypress/cypress/pages/workflow/workflowRecurring.js
+++ Cypress/cypress/pages/workflow/workflowRecurring.js
@@ -2,17 +2,17 @@
 
 Cypress.Commands.add("validateRecurringStats", () => {
     cy.get("[data-cy=statsHeatMap]")
-        .then(($div) => {
-            if ($div.find("[data-cy=statsHeatMapLoader]"))
-                return false;
-            else
-                return true;
-        }),
-        {
-            verbose: true,
-            interval: 500,
-            timeout: 60000,
-        }
+        .within(($div) => {
+            cy.waitUntil(() => {
+                    return $div.find("[data-cy=statsHeatMapLoader]");
+                },
+                {
+                    verbose: true,
+                    interval: 500,
+                    timeout: 60000,
+                }
+            );
+        });
     cy.get("[data-cy=statsHeatMap]")
         .within((el) => {
             cy.wrap(el)
@@ -21,17 +21,17 @@ Cypress.Commands.add("validateRecurringStats", () => {
         });
 
     cy.get("[data-cy=statsBarGraphDiv]")
-        .then(($div) => {
-            if ($div.find("[data-cy=statsBarGraphLoader]"))
-                return false;
-            else
-                return true;
-        }),
-        {
-            verbose: true,
-            interval: 500,
-            timeout: 60000,
-        }
+        .within(($div) => {
+            cy.waitUntil(() => {
+                    return $div.find("[data-cy=statsBarGraphLoader]");
+                },
+                {
+                    verbose: true,
+                    interval: 500,
+                    timeout: 60000,
+                }
+            );
+        });
     cy.get("[data-cy=statsBarGraph")
         .within((el) => {
             cy.wrap(el)
