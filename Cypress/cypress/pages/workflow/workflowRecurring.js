49621dbc7270ca5641e75283d49ec595dd59da60
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-11-17 08:24:03 +0530

    Replace waitUntil for stats graph and heatmap with should visible
    Signed-off-by: DhananjayPurohit <dhananjaypurohit7@gmail.com>

--- Cypress/cypress/pages/workflow/workflowRecurring.js
+++ Cypress/cypress/pages/workflow/workflowRecurring.js
@@ -1,18 +1,7 @@
 ///  Validate stats for recurring 
 
 Cypress.Commands.add("validateRecurringStats", () => {
-    cy.get("[data-cy=statsHeatMap]")
-        .within(($div) => {
-            cy.waitUntil(() => {
-                    return $div.find("[data-cy=statsHeatMapLoader]");
-                },
-                {
-                    verbose: true,
-                    interval: 500,
-                    timeout: 60000,
-                }
-            );
-        });
+    cy.get("[data-cy=statsHeatMap]").should("be.visible");
     cy.get("[data-cy=statsHeatMap]")
         .within((el) => {
             cy.wrap(el)
@@ -20,18 +9,7 @@ Cypress.Commands.add("validateRecurringStats", () => {
                 .click();
         });
 
-    cy.get("[data-cy=statsBarGraphDiv]")
-        .within(($div) => {
-            cy.waitUntil(() => {
-                    return $div.find("[data-cy=statsBarGraphLoader]");
-                },
-                {
-                    verbose: true,
-                    interval: 500,
-                    timeout: 60000,
-                }
-            );
-        });
+    cy.get("[data-cy=statsBarGraph]").should("be.visible");
     cy.get("[data-cy=statsBarGraph")
         .within((el) => {
             cy.wrap(el)
