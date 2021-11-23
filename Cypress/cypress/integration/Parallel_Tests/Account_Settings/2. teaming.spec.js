21dec6cd98ed689d0a6cef8740cbc742d992071b
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-11-24 00:32:21 +0530

    Refactoring Teaming tests.
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- Cypress/cypress/integration/Parallel_Tests/Account_Settings/2. teaming.spec.js
+++ Cypress/cypress/integration/Parallel_Tests/Account_Settings/2. teaming.spec.js
@@ -25,102 +25,129 @@ describe("Testing the Teaming section", () => {
     });
   });
 
-  it("Test case to check the Invitation Functionality (Invitation as a viewer)", () => {
-    //Creating a new user for this test case
-    cy.getCookie("litmus-cc-token").then((token) => {
-      cy.request({
-        method: "POST",
-        url: Cypress.env("authURL") + "/create",
-        body: {
-          email: "",
-          username: user1.username,
-          name: "",
-          password: user1.password,
-          role: "user",
-        },
-        headers: {
-          authorization: `Bearer ${token.value}`,
-        },
-      })
-        .its("status")
-        .should("eq", 200);
+  describe("Test case to check the Invitation Functionality (Invitation as a Viewer)", () => {
+    before("Clearing local storage & login as Admin", () => {
+      cy.clearCookie("litmus-cc-token");
+      indexedDB.deleteDatabase("localforage");
+      cy.requestLogin(AdminName, AdminPassword);
     });
 
-    cy.logout();
+    it("Creating a new user using API & logout", () => {
+      //Creating a new user for this test case
+      cy.getCookie("litmus-cc-token").then((token) => {
+        cy.request({
+          method: "POST",
+          url: Cypress.env("authURL") + "/create",
+          body: {
+            email: "",
+            username: user1.username,
+            name: "",
+            password: user1.password,
+            role: "user",
+          },
+          headers: {
+            authorization: `Bearer ${token.value}`,
+          },
+        })
+          .its("status")
+          .should("eq", 200);
+      });
+    });
 
-    // //Login as the intivation receipent
-    cy.requestLogin(user1.username, user1.password);
-    cy.getStarted(user1.password);
-    cy.validateScaffold();
-    cy.logout();
+    it("Logging in with newly created user & logout", () => {
+      // //Login as the intivation receipent (Project creation required for sending invitation)
+      cy.requestLogin(user1.username, user1.password);
+      cy.getStarted(user1.password);
+      cy.validateScaffold();
+      cy.wait(5000);
+      cy.logout();
+    });
 
-    // //Login again as Admin to confirm that member has been added.
+    it("Logging In as Admin again, invite the user as viewer", () => {
+      // //Login again as Admin
+      cy.requestLogin(AdminName, AdminPassword);
+
+      //Visit the teaming section and invite the newly created user as viewer"
+      cy.visit("/settings");
+      cy.get("[data-cy=teaming]").should("be.visible").click();
+      cy.get("[data-cy=toolBarComponent]").should("be.visible");
+      cy.inviteUser(user1.username, user1.role);
+    });
+
+    it("validate the invitation & logout", () => {
+      //Check if invitation got sent in the "Sent Tab"
+      cy.get("[data-cy=invitedTab]")
+        .find("span")
+        .eq(0)
+        .should("have.text", "1 Invited");
+      cy.get("[data-cy=invitedTab]").click();
+      cy.get("[data-cy=teamingSearch] input").clear().type(user1.username);
+      cy.validateSentInvite({
+        username: user1.username,
+        role: user1.role,
+        email: "",
+        status: "Pending",
+      });
+
+      cy.logout();
+    });
+
+    it("Logging in as user1, validate the invitation & Accept it", () => {
+      //Login again as the intivation receipent
+      cy.requestLogin(user1.username, user1.password);
+
+      //Go to Settings/Team section & accept the invite
+      cy.visit("/settings");
+      cy.get("[data-cy=teaming]").click();
+      cy.validateProjectsDetails({
+        totalProjects: 1,
+        ownedProjects: 1,
+        invitationsRecieved: 1,
+      });
+      cy.get("[data-cy=receivedTab]")
+        .find("span")
+        .eq(0)
+        .should("have.text", "1 Invitations");
+      cy.get("[data-cy=receivedTab]").click();
+      cy.get("[data-cy=receivedInvitationAccept] button").eq(0).click();
+      cy.validateProjectsDetails({
+        totalProjects: 2,
+        ownedProjects: 1,
+        invitationsRecieved: 0,
+      });
+      cy.logout();
+    });
+
+    it("Again Login as Admin & validate the member", () => {
+      //Login again as Admin to confirm that member has been added.
+      cy.requestLogin(AdminName, AdminPassword);
+
+      //Validate the user as a member of project
+      cy.visit("/settings");
+      cy.get("[data-cy=teaming]").click();
+      cy.get("[data-cy=teamingSearch] input").clear().type(user1.username);
+      cy.validateMember({
+        username: user1.username,
+        role: user1.role,
+        email: "",
+      });
+    });
+
+    it("RBAC Validations for Viewer", () => {
+      /// More tests for RBAC operations will be added here
+    });
+  });
+});
+
+describe("Test case to check the Invitation Functionality (Invitation as an Editor)", () => {
+  before("Clearing local storage & login as Admin", () => {
+    cy.clearCookie("litmus-cc-token");
+    indexedDB.deleteDatabase("localforage");
     cy.requestLogin(AdminName, AdminPassword);
-
-    //Visit the teaming section and invite the newly created user as viewer"
-    cy.visit("/settings");
-    cy.get("[data-cy=teaming]").should("be.visible").click();
-    cy.get("[data-cy=toolBarComponent]").should("be.visible");
-    cy.inviteUser(user1.username, user1.role);
-
-    //Check if invitation got sent in the "Sent Tab"
-    cy.get("[data-cy=invitedTab]")
-      .find("span")
-      .eq(0)
-      .should("have.text", "1 Invited");
-    cy.get("[data-cy=invitedTab]").click();
-    cy.get("[data-cy=teamingSearch] input").clear().type(user1.username);
-    cy.validateSentInvite({
-      username: user1.username,
-      role: user1.role,
-      email: "",
-      status: "Pending",
-    });
-
-    cy.logout();
-
-    //Login again as the intivation receipent
-    cy.requestLogin(user1.username, user1.password);
-
-    //Go to Settings/Team section & accept the invite
-    cy.visit("/settings");
-    cy.get("[data-cy=teaming]").click();
-    cy.validateProjectsDetails({
-      totalProjects: 1,
-      ownedProjects: 1,
-      invitationsRecieved: 1,
-    });
-    cy.get("[data-cy=receivedTab]")
-      .find("span")
-      .eq(0)
-      .should("have.text", "1 Invitations");
-    cy.get("[data-cy=receivedTab]").click();
-    cy.get("[data-cy=receivedInvitationAccept] button").eq(0).click();
-    cy.validateProjectsDetails({
-      totalProjects: 2,
-      ownedProjects: 1,
-      invitationsRecieved: 0,
-    });
-    cy.logout();
-
-    //Login again as Admin to confirm that member has been added.
-    cy.requestLogin(AdminName, AdminPassword);
-
-    //Validate the user as a member of project
-    cy.visit("/settings");
-    cy.get("[data-cy=teaming]").click();
-    cy.get("[data-cy=teamingSearch] input").clear().type(user1.username);
-    cy.validateMember({
-      username: user1.username,
-      role: user1.role,
-      email: "",
-    });
-
-    /// More tests for RBAC operations will be added here
   });
 
-  it("Test case to check the Invitation Functionality (Invitation as a Editor)", () => {
-    // Creating a new user for this test case
+  it("Creating a new user using API & logout", () => {
+    //Creating a new user for this test case
     cy.getCookie("litmus-cc-token").then((token) => {
       cy.request({
         method: "POST",
@@ -139,16 +166,19 @@ describe("Testing the Teaming section", () => {
         .its("status")
         .should("eq", 200);
     });
+  });
 
-    cy.logout();
-
-    // //Login as the intivation receipent
+  it("Logging in with newly created user & logout", () => {
+    // //Login as the intivation receipent (Project creation required for sending invitation)
     cy.requestLogin(user2.username, user2.password);
     cy.getStarted(user2.password);
     cy.validateScaffold();
+    cy.wait(5000);
     cy.logout();
+  });
 
-    // //Login again as Admin to confirm that member has been added.
+  it("Logging In as Admin again, invite the user as viewer", () => {
+    // //Login again as Admin
     cy.requestLogin(AdminName, AdminPassword);
 
     //Visit the teaming section and invite the newly created user as viewer"
@@ -156,7 +186,9 @@ describe("Testing the Teaming section", () => {
     cy.get("[data-cy=teaming]").should("be.visible").click();
     cy.get("[data-cy=toolBarComponent]").should("be.visible");
     cy.inviteUser(user2.username, user2.role);
+  });
 
+  it("validate the invitation & logout", () => {
     //Check if invitation got sent in the "Sent Tab"
     cy.get("[data-cy=invitedTab]")
       .find("span")
@@ -172,7 +204,9 @@ describe("Testing the Teaming section", () => {
     });
 
     cy.logout();
+  });
 
+  it("Logging in as user2, validate the invitation & Accept it", () => {
     //Login again as the intivation receipent
     cy.requestLogin(user2.username, user2.password);
 
@@ -196,7 +230,9 @@ describe("Testing the Teaming section", () => {
       invitationsRecieved: 0,
     });
     cy.logout();
+  });
 
+  it("Again Login as Admin & validate the member", () => {
     //Login again as Admin to confirm that member has been added.
     cy.requestLogin(AdminName, AdminPassword);
 
@@ -209,7 +245,9 @@ describe("Testing the Teaming section", () => {
       role: user2.role,
       email: "",
     });
+  });
 
+  it("RBAC Validations for Editor", () => {
     /// More tests for RBAC operations will be added here
   });
 });
