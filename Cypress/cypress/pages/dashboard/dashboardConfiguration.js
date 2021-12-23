6bdcb90b66a94bdb6caefa03e9db5c45d798f42e
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-12-24 02:52:27 +0530

    Add test for dashboard creation, configuration, deletion
    Signed-off-by: DhananjayPurohit <dhananjaypurohit7@gmail.com>

---
+++ Cypress/cypress/pages/dashboard/dashboardConfiguration.js
@@ -0,0 +1,15 @@
+/// ************************** Dashboard Configuration and Validation **********************
+
+Cypress.Commands.add("configureDashboard", (configureDashboard) => {
+    cy.get("[data-cy=inputDashboardName] input").clear().type(configureDashboard.name);
+    cy.get("[data-cy=agentName] [tabIndex=0]").should("have.text", configureDashboard.agent);
+    cy.get("[data-cy=selectDatasource]").click();
+    cy.get(`[data-cy=${configureDashboard.dataSource}]`).eq(0).click();
+    cy.get("[data-cy=inputDashboardType] input").should("have.value", configureDashboard.dashboardType);
+    cy.get("[data-cy=selectNamespaces]").clear().type(configureDashboard.namespace);
+    cy.get("li")
+        .eq(1)
+        .click();
+    cy.get("[data-cy=applicationType]").click();
+    cy.get(`[data-cy=${configureDashboard.applicationType}]`).click();
+});
