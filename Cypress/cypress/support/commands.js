fc6f4ad38510f7435a0a5362d49ea9a4215bfea7
Aman Dwivedi <54680709+Aman-Codes@users.noreply.github.com>
2022-04-27 20:52:55 +0530

    added chaos workflow related tests (#370)
    * added chaos workflow related tests
    
    Signed-off-by: Aman Dwivedi <aman.dwivedi5@gmail.com>
    
    * updated cluster tests
    
    Signed-off-by: Aman Dwivedi <aman.dwivedi5@gmail.com>
    Signed-off-by: Neelanjan Manna <neelanjan.manna@harness.io>

--- Cypress/cypress/support/commands.js
+++ Cypress/cypress/support/commands.js
@@ -145,202 +145,237 @@ Cypress.Commands.add("validateErrorMessage", (res, message) => {
 
 /*
   Project1: Admin(Owner), user1(Editor), user3(Viewer)
-  Project2: Admin(Owner), user2(Viewer)
+  Project2: user1(Owner), user2(Viewer)
 */
-Cypress.Commands.add("securityCheckSetup", () => {
-  const Projects = {
-    project1: "project1",
-    project2: "project2",
-  };
-  let project1Id, project2Id, user1Id, user2Id, user3Id, accessToken;
-  cy.requestLogin(user.AdminName, user.AdminPassword);
-  return cy
-    .getCookie("litmus-cc-token")
-    .then((token) => {
-      accessToken = token.value;
-      // create user1
-      const createUser1 = cy
-        .request({
-          method: "POST",
-          url: Cypress.env("authURL") + "/create",
-          headers: {
-            authorization: `Bearer ${accessToken}`,
-          },
-          body: { ...user.user1 },
-        })
-        .then((res) => {
-          user1Id = res.body._id;
-        });
+Cypress.Commands.add(
+  "securityCheckSetup",
+  (adminProjectId, adminProjectName) => {
+    const Projects = {
+      project1: adminProjectName,
+      project2: "user1's project",
+    };
+    let project1Id = adminProjectId,
+      project2Id,
+      user1Id,
+      user2Id,
+      user3Id,
+      adminAccessToken,
+      user1AccessToken;
+    cy.requestLogin(user.AdminName, user.AdminPassword);
+    return cy
+      .getCookie("litmus-cc-token")
+      .then((token) => {
+        adminAccessToken = token.value;
+        // create user1
+        const createUser1 = cy
+          .request({
+            method: "POST",
+            url: Cypress.env("authURL") + "/create",
+            headers: {
+              authorization: `Bearer ${adminAccessToken}`,
+            },
+            body: { ...user.user1 },
+          })
+          .then((res) => {
+            user1Id = res.body._id;
+          });
 
-      // create user2
-      const createUser2 = cy
-        .request({
-          method: "POST",
-          url: Cypress.env("authURL") + "/create",
-          headers: {
-            authorization: `Bearer ${accessToken}`,
-          },
-          body: { ...user.user2 },
-        })
-        .then((res) => {
-          user2Id = res.body._id;
-        });
+        // create user2
+        const createUser2 = cy
+          .request({
+            method: "POST",
+            url: Cypress.env("authURL") + "/create",
+            headers: {
+              authorization: `Bearer ${adminAccessToken}`,
+            },
+            body: { ...user.user2 },
+          })
+          .then((res) => {
+            user2Id = res.body._id;
+          });
 
-      // create user3
-      const createUser3 = cy
-        .request({
+        // create user3
+        const createUser3 = cy
+          .request({
+            method: "POST",
+            url: Cypress.env("authURL") + "/create",
+            headers: {
+              authorization: `Bearer ${adminAccessToken}`,
+            },
+            body: { ...user.user3 },
+          })
+          .then((res) => {
+            user3Id = res.body._id;
+          });
+        return Promise.all([createUser1, createUser2, createUser3]);
+      })
+      .then(() => {
+        // send invitation of project1 to user1 with Editor role
+        return cy.request({
           method: "POST",
-          url: Cypress.env("authURL") + "/create",
+          url: Cypress.env("authURL") + "/send_invitation",
           headers: {
-            authorization: `Bearer ${accessToken}`,
-          },
-          body: { ...user.user3 },
-        })
-        .then((res) => {
-          user3Id = res.body._id;
-        });
-
-      // create project1
-      const createProject1 = cy
-        .request({
-          method: "POST",
-          url: Cypress.env("authURL") + "/create_project",
-          headers: {
-            authorization: `Bearer ${accessToken}`,
+            authorization: `Bearer ${adminAccessToken}`,
           },
           body: {
-            project_name: Projects.project1,
+            project_id: project1Id,
+            user_id: user1Id,
+            role: "Editor",
           },
-        })
-        .then((res) => {
-          project1Id = res.body.data.ID;
         });
-      // create project2
-      const createProject2 = cy
-        .request({
+      })
+      .then(() => {
+        // send invitation of project1 to user3 with Viewer role
+        return cy.request({
           method: "POST",
-          url: Cypress.env("authURL") + "/create_project",
+          url: Cypress.env("authURL") + "/send_invitation",
           headers: {
-            authorization: `Bearer ${accessToken}`,
+            authorization: `Bearer ${adminAccessToken}`,
           },
           body: {
-            project_name: Projects.project2,
+            project_id: project1Id,
+            user_id: user3Id,
+            role: "Viewer",
           },
-        })
-        .then((res) => {
-          project2Id = res.body.data.ID;
         });
-      return Promise.all([
-        createUser1,
-        createUser2,
-        createUser3,
-        createProject1,
-        createProject2,
-      ]);
-    })
-    .then(() => {
-      // send invitation of project1 to user1 with Editor role
-      return cy.request({
-        method: "POST",
-        url: Cypress.env("authURL") + "/send_invitation",
-        headers: {
-          authorization: `Bearer ${accessToken}`,
-        },
-        body: {
-          project_id: project1Id,
-          user_id: user1Id,
-          role: "Editor",
-        },
+      })
+      .then(() => {
+        cy.requestLogin(user.user1.username, user.user1.password);
+        return cy.getCookie("litmus-cc-token");
+      })
+      .then((token) => {
+        user1AccessToken = token.value;
+        // create project2
+        return cy
+          .request({
+            method: "POST",
+            url: Cypress.env("authURL") + "/create_project",
+            headers: {
+              authorization: `Bearer ${user1AccessToken}`,
+            },
+            body: {
+              project_name: Projects.project2,
+            },
+          })
+          .then((res) => {
+            project2Id = res.body.data.ID;
+          });
+      })
+      .then(() => {
+        // send invitation of project2 to user2 with Viewer role
+        return cy.request({
+          method: "POST",
+          url: Cypress.env("authURL") + "/send_invitation",
+          headers: {
+            authorization: `Bearer ${user1AccessToken}`,
+          },
+          body: {
+            project_id: project2Id,
+            user_id: user2Id,
+            role: "Viewer",
+          },
+        });
+      })
+      .then(() => {
+        cy.logout();
+        cy.requestLogin(user.user3.username, user.user3.password);
+        return cy.getCookie("litmus-cc-token");
+      })
+      .then((token) => {
+        return cy.request({
+          method: "POST",
+          url: Cypress.env("authURL") + "/accept_invitation",
+          headers: {
+            authorization: `Bearer ${token.value}`,
+          },
+          body: {
+            project_id: project1Id,
+            user_id: user3Id,
+          },
+        });
+      })
+      .then(() => {
+        cy.logout();
+        cy.requestLogin(user.user1.username, user.user1.password);
+        return cy.getCookie("litmus-cc-token");
+      })
+      .then((token) => {
+        return cy.request({
+          method: "POST",
+          url: Cypress.env("authURL") + "/accept_invitation",
+          headers: {
+            authorization: `Bearer ${token.value}`,
+          },
+          body: {
+            project_id: project1Id,
+            user_id: user1Id,
+          },
+        });
+      })
+      .then(() => {
+        cy.logout();
+        cy.requestLogin(user.user2.username, user.user2.password);
+        return cy.getCookie("litmus-cc-token");
+      })
+      .then((token) => {
+        return cy.request({
+          method: "POST",
+          url: Cypress.env("authURL") + "/accept_invitation",
+          headers: {
+            authorization: `Bearer ${token.value}`,
+          },
+          body: {
+            project_id: project2Id,
+            user_id: user2Id,
+          },
+        });
+      })
+      .then(() => {
+        return cy.logout();
+      })
+      .then(() => {
+        return { project1Id, project2Id, user1Id, user2Id, user3Id };
       });
-    })
-    .then(() => {
-      // send invitation of project1 to user3 with Viewer role
-      return cy.request({
-        method: "POST",
-        url: Cypress.env("authURL") + "/send_invitation",
-        headers: {
-          authorization: `Bearer ${accessToken}`,
-        },
-        body: {
-          project_id: project1Id,
-          user_id: user3Id,
-          role: "Viewer",
-        },
+  }
+);
+
+Cypress.Commands.add(
+  "createAgent",
+  (agentName, namespace = "n1", projectId = "") => {
+    return cy
+      .exec(
+        "kubectl -n litmus get -o jsonpath='{.spec.ports[0].nodePort}' services litmusportal-frontend-service"
+      )
+      .then((res) => {
+        const url = "http://localhost:" + res.stdout;
+        return cy.exec(
+          `litmusctl config set-account  --endpoint '${url}' --password 'litmus' --username 'admin'`
+        );
+      })
+      .then((res) => {
+        return cy.exec(
+          `kubectl get ns | awk '/${namespace}/' | awk '{print $1}'`
+        );
+      })
+      .then((res) => {
+        if (res.stdout === "") {
+          return cy.exec(`kubectl create ns ${namespace}`);
+        } else {
+          return cy.exec(
+            `kubectl delete ns ${namespace} && kubectl create ns ${namespace}`
+          );
+        }
+      })
+      .then((res) => {
+        return cy.exec(
+          `litmusctl create agent --agent-name='${agentName}' --project-id='${projectId}' --namespace='${namespace}'  --installation-mode='namespace' --non-interactive`
+        );
+      })
+      .then((res) => {
+        return cy.task("waitForAgent", agentName);
+      })
+      .then((res) => {
+        console.log(res);
       });
-    })
-    .then(() => {
-      // send invitation of project2 to user2 with Viewer role
-      return cy.request({
-        method: "POST",
-        url: Cypress.env("authURL") + "/send_invitation",
-        headers: {
-          authorization: `Bearer ${accessToken}`,
-        },
-        body: {
-          project_id: project2Id,
-          user_id: user2Id,
-          role: "Viewer",
-        },
-      });
-    })
-    .then(() => {
-      cy.logout();
-      cy.requestLogin(user.user3.username, user.user3.password);
-      return cy.getCookie("litmus-cc-token");
-    })
-    .then((token) => {
-      return cy.request({
-        method: "POST",
-        url: Cypress.env("authURL") + "/accept_invitation",
-        headers: {
-          authorization: `Bearer ${token.value}`,
-        },
-        body: {
-          project_id: project1Id,
-          user_id: user3Id,
-        },
-      });
-    })
-    .then(() => {
-      cy.logout();
-      cy.requestLogin(user.user1.username, user.user1.password);
-      return cy.getCookie("litmus-cc-token");
-    })
-    .then((token) => {
-      return cy.request({
-        method: "POST",
-        url: Cypress.env("authURL") + "/accept_invitation",
-        headers: {
-          authorization: `Bearer ${token.value}`,
-        },
-        body: {
-          project_id: project1Id,
-          user_id: user1Id,
-        },
-      });
-    })
-    .then(() => {
-      cy.logout();
-      cy.requestLogin(user.user2.username, user.user2.password);
-      return cy.getCookie("litmus-cc-token");
-    })
-    .then((token) => {
-      return cy.request({
-        method: "POST",
-        url: Cypress.env("authURL") + "/accept_invitation",
-        headers: {
-          authorization: `Bearer ${token.value}`,
-        },
-        body: {
-          project_id: project2Id,
-          user_id: user2Id,
-        },
-      });
-    })
-    .then(() => {
-      return cy.logout();
-    })
-    .then(() => {
-      return { project1Id, project2Id, user1Id, user2Id, user3Id };
-    });
-});
+  }
+);
