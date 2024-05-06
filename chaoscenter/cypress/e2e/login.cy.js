542ea5d7154dee85d7490792f9cce708c9715181
Dhanush0369 <dhanush789245@gmail.com>
2024-05-06 15:07:47 +0530

    Delete Previous Files
    Signed-off-by: Dhanush0369 <dhanush789245@gmail.com>

--- chaoscenter/cypress/e2e/login.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Testing login page of chaoscenter', () => {
-  it('testing login with valid data', () => {
-    cy.login(Cypress.env('username'),Cypress.env('password'));
-  });
-
-  it('testing login thorugh REST API', () => {
-    cy.requestLogin(Cypress.env('username'),Cypress.env('password'));
-  });
-});
