fa94ebb9799b48646f3ff272edf441a92ad36e64
Dhanush0369 <dhanush789245@gmail.com>
2024-05-22 14:44:03 +0530

    Update E2E-chaoscenter workflow and cypress config file
    Signed-off-by: Dhanush0369 <dhanush789245@gmail.com>

--- chaoscenter/cypress.config.js
+++ chaoscenter/cypress.config.js
@@ -2,11 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000", // default value
-    setupNodeEvents(on, config) {
-      config.env.baseUrl = process.env.CYPRESS_baseUrl || config.env.baseUrl;
-      return config;
-    },
+    baseUrl: process.env.CYPRESS_baseUrl || "http://localhost:3000", // default value
     requestTimeout: 60000,
     responseTimeout: 60000
   },
